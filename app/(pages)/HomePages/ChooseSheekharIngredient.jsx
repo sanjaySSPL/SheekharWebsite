d34931d7694d56e8f00514c4c29dfaffcbd1ec17
16gsanket <16gsanket@gmail.com>
2025-06-28 16:33:45 +0530

    feat: fixed the ingredients part

--- app/(pages)/HomePages/ChooseSheekharIngredient.jsx
+++ app/(pages)/HomePages/ChooseSheekharIngredient.jsx
@@ -22,70 +22,80 @@ function ChooseSheekharIngredient() {
       alt: 'Process Compatibility',
       x: isMobile ? 90 : 280,
       y: isMobile ? -250 : -300,
+      color:'#EFF9FF'
     },
     {
       src: 'https://res.cloudinary.com/dipc3ow9z/image/upload/v1750747226/ImprovedStability_Appearance_we31ol.svg',
       alt: 'Improved Stability',
       x: isMobile ? -150 : -510,
       y: isMobile ? -280 : -350,
+      color:"#EFF9FF"
     },
     {
       src: 'https://res.cloudinary.com/dipc3ow9z/image/upload/v1750747225/goodQuality_fl7kns.svg',
       alt: 'Good Quality',
       x: isMobile ? 120 : -90,
       y: isMobile ? -100 : -400,
+      color:"#FFEFFA"
     },
     {
       src: 'https://res.cloudinary.com/dipc3ow9z/image/upload/v1750747225/HigherYieldLowerWastage_orrobj.svg',
-      alt: 'Higher Yield',
+      alt: 'Higher Yield Lower Wastage',
       x: isMobile ? -90 : -550,
       y: isMobile ? -30 : -100,
+      color:"#f4ffef"
     },
     {
       src: 'https://res.cloudinary.com/dipc3ow9z/image/upload/v1750747224/BetterTexture_Mouthfeel_bgmb4n.svg',
       alt: 'Better Texture & Mouthfeel',
       x: isMobile ? 80 : 360,
       y: isMobile ? 10 : -130,
+      color:"#F2FFEF"
     },
     {
       src: 'https://res.cloudinary.com/dipc3ow9z/image/upload/v1750747223/CustomizedProblemSpecificSOlutions_sezw6p.svg',
       alt: 'Customized, Problem-Specific Solutions',
       x: isMobile ? -70 : -500,
       y: isMobile ? 100 : 150,
+      color:"#F9EFFF"
     },
     {
       src: 'https://res.cloudinary.com/dipc3ow9z/image/upload/v1750747224/StrongerConsumerRespomse_RevenueUplift_t1fwyp.svg',
-      alt: 'Stronger Consumer Response',
+      alt: 'Stronger Consumer Response & Revenue Uplift',
       x: isMobile ? 60 : 500,
       y: isMobile ? 160 : 500,
+      color:"#ffefef"
     },
     {
       src: 'https://res.cloudinary.com/dipc3ow9z/image/upload/v1750747223/CleanLabel_HealthFocused_vnqzqu.svg',
       alt: 'Clean Label & Health-Focused',
       x: isMobile ? -40 : -120,
       y: isMobile ? 160 : 300,
+      color:"#f0effe"
     },
     {
       src: 'https://res.cloudinary.com/dipc3ow9z/image/upload/v1750747223/PremiumProduct_ecxrf8.svg',
       alt: 'Premium Product',
       x: isMobile ? 30 : 300,
       y: isMobile ? 220 : 200,
+      color:"#feffef"
     },
   ];
 
   return (
     <div ref={ref} className="h-[100dvh] md:h-[1000px] relative overflow-hidden">
-      <div className="absolute inset-0 z-10">
+      <div className="absolute inset-0 z-20">
         <div className="relative h-full w-full">
           {items.map((item, idx) => (
             <motion.div
               key={idx}
-              initial={{ x: 0, y: 0, opacity: 0.8 }}
+              initial={{ x: 0, y: 0, opacity: 1 }}
               animate={
                 inView
                   ? { x: item.x, y: item.y, opacity: 1 }
                   : { x: 0, y: 0, opacity: 0 }
               }
+              
               transition={{ duration: 1, delay: idx * 0.1 }}
               className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2"
             >
@@ -96,13 +106,18 @@ function ChooseSheekharIngredient() {
                 height={isMobile ? 80 : 100}
                 className="rounded shadow-md mx-auto hover:scale-110 transition-all duration-1000"
               />
-              <p className="text-center mt-2 text-[12px] md:text-text_blue md:text-[20px] ">{item.alt}</p>
+              <p 
+                className="text-center mt-2 text-[12px] md:text-text_blue md:text-[20px] px-4 py-1 rounded-full"
+                style={{ backgroundColor: item.color }}
+              >
+                {item.alt}
+              </p>
             </motion.div>
           ))}
         </div>
       </div>
 
-      <div className="w-full md:w-[688px] absolute z-20 top-1/2 -translate-y-1/2 md:left-1/2 md:top-1/2 md:-translate-y-1/2 transform md:-translate-x-1/2">
+      <div className="w-full md:w-[688px] absolute z-10 top-1/2 -translate-y-1/2 md:left-1/2 md:top-1/2 md:-translate-y-1/2 transform md:-translate-x-1/2">
         <h2 className="md:text-[36px] sm:text-[20px] text-[30px] text-center text-text_blue font-inter italic mx-10 md:mt-[32px] mt-[50px] mb-[50px] md:mb-[40px]">
           Choose Sheekhar Ingredient
         </h2>
